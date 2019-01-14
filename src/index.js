4f4d2dc7bc199d76f9defe69513b07d2690d089f
jriv95 <joel.rivera7995@gmail.com>
2019-01-14 13:44:09 -0500

    Steps 1-4

--- src/index.js
+++ src/index.js
@@ -1,8 +1,47 @@
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
 let addToy = false
+const toyCollection = document.querySelector('#toy-collection')
+const inputButton = document.getElementById('input-button')
 
 // YOUR CODE HERE
+document.addEventListener('DOMContentLoaded', function() {
+  fetch('http://localhost:3000/toys')
+  .then(object => object.json())
+  .then(toysParsedObject => {
+    toyCard(toysParsedObject)
+  })
+})
+
+
+const newToy = (e) => {
+  e.preventDefault()
+  const toyName = document.querySelector('#name-value').value
+  const toyImage = document.querySelector('#image-value').value
+
+  const inputValue = {
+    name: toyName,
+    image: toyImage,
+    likes: 0
+  }
+
+  fetch(`http://localhost:3000/toys`, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(inputValue)
+  })
+  .then(response => response.json())
+  .then(parsedObject => console.log(parsedObject))
+
+
+}
+
+inputButton.addEventListener('click', newToy)
+
+
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
@@ -17,3 +56,16 @@ addBtn.addEventListener('click', () => {
 
 
 // OR HERE!
+const toyCard = (object) => {
+  object.forEach(toy => {
+    const toyDiv = document.createElement('div')
+    console.log(toyDiv)
+    toyDiv.innerHTML = `<div class="card">
+      <h2>${toy.name}</h2>
+      <img src=${toy.image} class="toy-avatar" />
+      <p>${toy.likes} </p>
+      <button class="like-btn">Like <3</button>
+    </div>`
+    toyCollection.append(toyDiv)
+  })
+}
