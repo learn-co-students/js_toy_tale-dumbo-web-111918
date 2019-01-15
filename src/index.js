437e49510e3284a264a2bc2c3e3fd39750443c60
jriv95 <joel.rivera7995@gmail.com>
2019-01-15 18:30:46 -0500

    Solution

--- src/index.js
+++ src/index.js
@@ -1,48 +1,94 @@
+// YOUR CODE HERE
+const toyCollection = document.querySelector('#toy-collection')
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
+const inputbtn = document.querySelector('#input-button')
+const nameInput = document.querySelector('#name-value').value
+const imageUrl = document.querySelector('#image-value').value
+const addToyForm = document.querySelector('.add-toy-form')
 let addToy = false
-const toyCollection = document.querySelector('#toy-collection')
-const inputButton = document.getElementById('input-button')
 
-// YOUR CODE HERE
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', e => {
   fetch('http://localhost:3000/toys')
-  .then(object => object.json())
-  .then(toysParsedObject => {
-    toyCard(toysParsedObject)
-  })
+  .then(res => res.json())
+  .then(toys => showToys(toys))
 })
 
-// check to see if a function is invoked here or not to make an
-// event listener on the inputButton
-
-const addNewToy = (e) => {
-  e.preventDefault()
-  const nameInputValue = document.querySelector('#name-value').value
-  const imageInputValue = document.querySelector('#image-value').value
-
-  inputValue = {
-    name: nameInputValue,
-    image: imageInputValue,
-    likes: 0
-  }
-
-  fetch('http://localhost:3000/toys', {
-    method: "POST",
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(inputValue)
+const showToys = (toys) => {
+  toys.forEach(toy => {
+    toyCollection.innerHTML += ` <div class="card" data-id="${toy.id}">
+    <h2>${toy.name}</h2>
+    <img src=${toy.image} class="toy-avatar" />
+    <p> ${toy.likes} </p>
+    <button class="like-btn">Like <3</button>
+  </div>`
   })
-  .then(res => res.json())
-  .then(data => console.log(data))
-
 }
 
-inputButton.addEventListener('click', addNewToy)
+  addToyForm.addEventListener('submit', e => {
+    e.preventDefault()
+
+    newToyData = {
+      name: nameInput,
+      image: imageUrl,
+      likes: 0
+    }
+
+    fetch('http://localhost:3000/toys', {
+      method: 'POST',
+      body: JSON.stringify(newToyData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(res => res.json())
+    .then(newToy => {
+      addNewToy(newToy)
+    })
+  })
 
 
-// ADD MORE CODE
+
+const addNewToy = (toy) => {
+  toyCollection.innerHTML += ` <div class="card" data-id="${toy.id}">
+  <h2>${toy.name}</h2>
+  <img src=${toy.image} class="toy-avatar" />
+  <p> ${toy.likes} </p>
+  <button class="like-btn">Like <3</button>
+</div>`
+}
+
+
+toyCollection.addEventListener('click', e => {
+  if(e.target.classList.contains('like-btn')) {
+    const toyId = e.target.parentNode.getAttribute('data-id')
+    const p = e.target.parentNode.querySelector('p')
+    const numOfLikes = parseInt(p.innerText)
+
+    fetch(`http://localhost:3000/toys/${toyId}`, {
+      method: 'PATCH',
+      body: JSON.stringify({
+        likes: numOfLikes + 1
+      }),
+      headers: {
+        'Content-Type': "application/json"
+      }
+    })
+    .then(res => res.json())
+    .then(toyObject => p.innerText = toyObject.likes)
+
+  }
+})
+
+
+// const likeToy = (id) => {
+//   const id = document.querySelector()
+//   id.addEventListener('click', e => {
+//     console.log(e.target)
+//   })
+// }
+
+
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
@@ -54,44 +100,3 @@ addBtn.addEventListener('click', () => {
     toyForm.style.display = 'none'
   }
 })
-
-
-// OR HERE!
-const toyCard = (object) => {
-  object.forEach(toy => {
-    const toyDiv = document.createElement('div')
-    toyDiv.innerHTML = `<div class="card">
-      <h2>${toy.name}</h2>
-      <img src=${toy.image} class="toy-avatar" />
-      <p>${toy.likes} </p>
-      <button class="like-btn">Like <3</button>
-    </div>`
-    toyCollection.append(toyDiv)
-  })
-}
-
-
-// const createNewToy = (e) => {
-//   e.preventDefault();
-//   const nameValueInput = document.querySelector('#name-value').value
-//   const imageValueInput = document.querySelector('#image-value').value
-//
-//   const inputValue = {
-//     name: nameValueInput,
-//     image: imageValueInput,
-//     likes: 0
-//   }
-//
-//   fetch('http://localhost:3000/toys', {
-//     method: 'POST',
-//     body: JSON.stringify(inputValue),
-//     headers: {
-//       'Content-Type': 'application/json'
-//     }
-//   })
-//   .then(res => res.json())
-//   .then(data => console.log(data))
-//
-// }
-//
-// inputButton.addEventListener('click', createNewToy)
