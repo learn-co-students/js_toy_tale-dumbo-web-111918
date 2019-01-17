64e00ccc5ce66853efe9e461e07d5e1208a4d186
jriv95 <joel.rivera7995@gmail.com>
2019-01-17 07:47:29 -0500

    Did it again. Looking for speed

--- src/index.js
+++ src/index.js
@@ -1,93 +1,71 @@
-// YOUR CODE HERE
-const toyCollection = document.querySelector('#toy-collection')
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
-const inputbtn = document.querySelector('#input-button')
-const nameInput = document.querySelector('#name-value').value
-const imageUrl = document.querySelector('#image-value').value
+const toyCollection = document.querySelector('#toy-collection')
 const addToyForm = document.querySelector('.add-toy-form')
 let addToy = false
 
+// YOUR CODE HERE
 document.addEventListener('DOMContentLoaded', e => {
   fetch('http://localhost:3000/toys')
   .then(res => res.json())
-  .then(toys => showToys(toys))
-})
+  .then(toys => toys.forEach(toy => showToy(toy)))
 
-const showToys = (toys) => {
-  toys.forEach(toy => {
-    toyCollection.innerHTML += ` <div class="card" data-id="${toy.id}">
+}) //END OF DOMContentLoaded
+
+const showToy = (toy) => {
+  toyCollection.innerHTML += `<div class="card" data-id="${toy.id}">
     <h2>${toy.name}</h2>
     <img src=${toy.image} class="toy-avatar" />
     <p> ${toy.likes} </p>
     <button class="like-btn">Like <3</button>
   </div>`
-  })
 }
 
-  addToyForm.addEventListener('submit', e => {
-    e.preventDefault()
+addToyForm.addEventListener('submit', e => {
+  e.preventDefault()
+  const nameValue = document.querySelector('#name-value').value
+  const imageValue = document.querySelector('#image-value').value
 
-    newToyData = {
-      name: nameInput,
-      image: imageUrl,
+  const toyData = {
+      name: nameValue,
+      image: imageValue,
       likes: 0
+  }
+
+  fetch('http://localhost:3000/toys', {
+    method: 'POST',
+    body: JSON.stringify(toyData),
+    headers: {
+      'Content-Type': 'application/json'
     }
-
-    fetch('http://localhost:3000/toys', {
-      method: 'POST',
-      body: JSON.stringify(newToyData),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(res => res.json())
-    .then(newToy => {
-      addNewToy(newToy)
-    })
   })
+  .then(res => res.json())
+  .then(toy => showToy(toy))
 
-
-
-const addNewToy = (toy) => {
-  toyCollection.innerHTML += ` <div class="card" data-id="${toy.id}">
-  <h2>${toy.name}</h2>
-  <img src=${toy.image} class="toy-avatar" />
-  <p> ${toy.likes} </p>
-  <button class="like-btn">Like <3</button>
-</div>`
-}
-
+})
 
 toyCollection.addEventListener('click', e => {
   if(e.target.classList.contains('like-btn')) {
-    const toyId = e.target.parentNode.getAttribute('data-id')
+    const dataId = e.target.parentNode.getAttribute('data-id')
     const p = e.target.parentNode.querySelector('p')
-    const numOfLikes = parseInt(p.innerText)
+    const pValue = p.innerText
+    const numOfLikes = parseInt(pValue)
 
-    fetch(`http://localhost:3000/toys/${toyId}`, {
-      method: 'PATCH',
-      body: JSON.stringify({
-        likes: numOfLikes + 1
-      }),
-      headers: {
-        'Content-Type': "application/json"
-      }
-    })
-    .then(res => res.json())
-    .then(toyObject => p.innerText = toyObject.likes)
+  fetch(`http://localhost:3000/toys/${dataId}`, {
+    method: 'PATCH',
+    body: JSON.stringify({
+      likes: numOfLikes + 1
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(res => res.json())
+  .then(toy => p.innerText = toy.likes)
 
   }
+
 })
-
-
-// const likeToy = (id) => {
-//   const id = document.querySelector()
-//   id.addEventListener('click', e => {
-//     console.log(e.target)
-//   })
-// }
-
 
 
 addBtn.addEventListener('click', () => {
@@ -100,3 +78,6 @@ addBtn.addEventListener('click', () => {
     toyForm.style.display = 'none'
   }
 })
+
+
+// OR HERE!
